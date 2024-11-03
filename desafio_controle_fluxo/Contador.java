96ff7c5542940868c68c21d688785cb54614b3e9
Jean Jerri <jeanjerri2022@gmail.com>
2024-11-03 10:39:15 -0300

    Desafio Controle de Fluxo (IntelliJ)

---
+++ desafio_controle_fluxo/Contador.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = entrada.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = entrada.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
